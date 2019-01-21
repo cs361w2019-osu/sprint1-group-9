250fe1dca308ab1129ad31596618373a36f0176e
jimmeycool <keysm@oregonstate.edu>
2019-01-20 17:20:28 -0800

    - overrode equals method for square - added basic attack status method

--- src/main/java/cs361/battleships/models/Board.java
+++ src/main/java/cs361/battleships/models/Board.java
@@ -32,8 +32,38 @@ public class Board {
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
 	public Result attack(int x, char y) {
-		//TODO Implement
-		return null;
+
+		// create square and result default
+		var firePos = new Square(x, y);
+		var result = new Result(firePos);
+		result.setResult(AtackStatus.MISS);
+
+        // search if there are any matching ship pos
+		var shipOp = ships.stream().filter(ship ->
+                ship.getOccupiedSquares().stream().anyMatch(sPos ->
+                        sPos.equals(firePos))).findAny();
+
+		// if ship present, mark hit and check if its a sink
+		shipOp.ifPresent(ship -> {
+		    result.setShip(ship);
+
+		    //check if sink
+		    if( ship.getOccupiedSquares().stream().allMatch(sPos ->
+                    attacks.stream().anyMatch(att -> sPos.equals(att.getLocation()))))
+		        result.setResult(AtackStatus.SUNK);
+		    else
+		        result.setResult(AtackStatus.HIT);
+        });
+
+		// check if game ends
+        if( ships.stream().allMatch(ship ->
+                ship.getOccupiedSquares().stream().allMatch(sPos ->
+                        attacks.stream().anyMatch(att -> sPos.equals(att.getLocation())))) )
+            result.setResult(AtackStatus.SURRENDER);
+
+
+		// return val
+		return result;
 	}
 
 	public List<Ship> getShips() {
@@ -46,12 +76,10 @@ public class Board {
 	}
 
 	public List<Result> getAttacks() {
-		//TODO implement
-		return null;
+		return attacks;
 	}
 
 	public void setAttacks(List<Result> attacks) {
-        //TODO implement
-
+	    this.attacks = attacks;
 	}
 }
