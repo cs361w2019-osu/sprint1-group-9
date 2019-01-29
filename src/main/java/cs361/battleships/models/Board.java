02f911069b1c8e7e755fe39cf78924d632eb1c48
Nathan127 <32209187+Nathan127@users.noreply.github.com>
2019-01-29 15:57:57 -0800

    Uploading Sprint 2 Code

--- src/main/java/cs361/battleships/models/Board.java
+++ src/main/java/cs361/battleships/models/Board.java
@@ -4,183 +4,74 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class Board
-{
-	private List<Ship> ships;
-	private List<Result> attacks;
+public class Board {
+
+	@JsonProperty private List<Ship> ships;
+	@JsonProperty private List<Result> attacks;
 
 	/*
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
-	public Board()
-	{
-		this.ships = new ArrayList<>();
-		this.attacks = new ArrayList<>();
+	public Board() {
+		ships = new ArrayList<>();
+		attacks = new ArrayList<>();
 	}
 
 	/*
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
-	public boolean placeShip(Ship ship, int x, char y, boolean isVertical)
-	{
-		System.out.println("Attempting to place ship...");
-		int shipSize = ship.getSize();
-
-		System.out.println("Validating ship type " + ship.getKind() + "...");
-		if (this.getShips().size() > 0)
-		{
-			if (!validShipType(ship)) return false;
+	public boolean placeShip(Ship ship, int x, char y, boolean isVertical) {
+		if (ships.size() >= 3) {
+			return false;
 		}
-
-		System.out.println("Validating ship location " + x + y + "...");
-		if (validLocation(shipSize, x, y, isVertical))
-		{
-			System.out.println("Success! Adding ship!");
-			ship.populateSquares(x, y, isVertical);
-			this.ships.add(ship);
-			return true;
+		if (ships.stream().anyMatch(s -> s.getKind().equals(ship.getKind()))) {
+			return false;
 		}
-		return false;
+		final var placedShip = new Ship(ship.getKind());
+		placedShip.place(y, x, isVertical);
+		if (ships.stream().anyMatch(s -> s.overlaps(placedShip))) {
+			return false;
+		}
+		if (placedShip.getOccupiedSquares().stream().anyMatch(s -> s.isOutOfBounds())) {
+			return false;
+		}
+		ships.add(placedShip);
+		return true;
 	}
 
 	/*
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
 	public Result attack(int x, char y) {
+		Result attackResult = attack(new Square(x, y));
+		attacks.add(attackResult);
+		return attackResult;
+	}
 
-		System.out.println("Attacking...");
-		// create square and result default
-		var firePos = new Square(x, y);
-		var result = new Result(firePos);
-		result.setResult(AtackStatus.MISS);
-		attacks.add(result);
-
-		System.out.println("Checking if attack hit anything...");
-		// search if there are any matching ship pos
-		var shipOp = ships.stream().filter(ship ->
-				ship.getOccupiedSquares().stream().anyMatch(sPos ->
-						sPos.equals(firePos))).findAny();
-
-		// if ship present, mark hit and check if its a sink
-		shipOp.ifPresent(ship -> {
-			System.out.println("Hit!");
-			result.setShip(ship);
-
-			//check if sink
-			System.out.println("Checking if sink...");
-			if( ship.getOccupiedSquares().stream().allMatch(sPos ->
-					attacks.stream().anyMatch(att -> sPos.equals(att.getLocation())))) {
-				System.out.println("Sunk!");
-				result.setResult(AtackStatus.SUNK);
-			}
-
-			else
-				result.setResult(AtackStatus.HIT);
-		});
-
-		System.out.println("Checking for endGame...");
-		// check if game ends
-		if( ships.stream().allMatch(ship ->
-				ship.getOccupiedSquares().stream().allMatch(sPos ->
-						attacks.stream().anyMatch(att -> sPos.equals(att.getLocation())))) ) {
-			System.out.println("Game has ended!");
-			result.setResult(AtackStatus.SURRENDER);
+	private Result attack(Square s) {
+		if (attacks.stream().anyMatch(r -> r.getLocation().equals(s))) {
+			var attackResult = new Result(s);
+			attackResult.setResult(AtackStatus.INVALID);
+			return attackResult;
 		}
-
-		// return val
-		System.out.println("Returning attack result " + result.getResult());
-		return result;
-	}
-
-	public List<Ship> getShips()
-	{
-		return this.ships;
-	}
-
-	public void setShips(List<Ship> ships)
-	{
-		this.ships = ships;
-	}
-
-	public List<Result> getAttacks()
-	{
-		return attacks;
-	}
-
-	public void setAttacks(List<Result> attacks)
-	{
-		this.attacks = attacks;
-	}
-
-	/*
-	This function takes proposed ship coords and returns true or false based on whether the proposed location is valid
-	 */
-	private boolean validLocation(int size, int x, char y, boolean vertical)
-	{
-		List<Square> allProposedSquares = new ArrayList<>();
-		if (vertical)
-		{
-			if (x + size - 1 > 10 || (int) y > 74)
-			{
-				return false;
-			}
-			for (int i = 0; i < size; i++)
-			{
-				allProposedSquares.add(new Square(x + i, y));
+		var shipsAtLocation = ships.stream().filter(ship -> ship.isAtLocation(s)).collect(Collectors.toList());
+		if (shipsAtLocation.size() == 0) {
+			var attackResult = new Result(s);
+			return attackResult;
+		}
+		var hitShip = shipsAtLocation.get(0);
+		var attackResult = hitShip.attack(s.getRow(), s.getColumn());
+		if (attackResult.getResult() == AtackStatus.SUNK) {
+			if (ships.stream().allMatch(ship -> ship.isSunk())) {
+				attackResult.setResult(AtackStatus.SURRENDER);
 			}
 		}
-		else
-		{
-			if ((int) y + size - 1 > 74 || x > 10)
-			{
-				return false;
-			}
-			for (int i = 0; i < size; i++)
-			{
-				allProposedSquares.add(new Square(x, (char) ((int) y + 1)));
-			}
-		}
-		//if max range is outside grid, return false;
-		//now checking if new ship would overlap with existing ships
-		if (this.getShips().size() > 0)
-		{
-			for (Ship ships : this.getShips())
-			{
-				for (Square sq : ships.getOccupiedSquares())
-				{
-					for (int i = 0; i < allProposedSquares.size(); i++)
-					{
-						if (isSquareConflict(allProposedSquares.get(i), sq))
-						{
-							return false;
-						}
-					}
-				}
-			}
-		}
-		//otherwise we make it through the tests and return true
-		return true;
+		return attackResult;
 	}
 
-	private boolean validShipType(Ship ship)
-	{
-		for (Ship ships : this.getShips())
-		{
-			if (ship.getKind().equals(ships.getKind())) return false;
-		}
-		return true;
-	}
-
-	private boolean isSquareConflict(Square sq1, Square sq2)
-	{
-		if (sq1.getRow() == sq2.getRow() && sq1.getColumn() == sq2.getColumn())
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
+	List<Ship> getShips() {
+		return ships;
 	}
 }
--- src/main/java/cs361/battleships/models/Game.java
+++ src/main/java/cs361/battleships/models/Game.java
@@ -17,36 +17,17 @@ public class Game {
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
     public boolean placeShip(Ship ship, int x, char y, boolean isVertical) {
-        System.out.println("");
-        System.out.println("");
-        System.out.println("A place ship request is in!");
-        System.out.println("Player is adding ship...");
         boolean successful = playersBoard.placeShip(ship, x, y, isVertical);
-        if (!successful) {
-            System.out.println("Invalid placement");
+        if (!successful)
             return false;
-        }
 
         boolean opponentPlacedSuccessfully;
         do {
             // AI places random ships, so it might try and place overlapping ships
             // let it try until it gets it right
-            System.out.println("Enemy is adding ship...");
             opponentPlacedSuccessfully = opponentsBoard.placeShip(ship, randRow(), randCol(), randVertical());
-            System.out.println("Placement was " + opponentPlacedSuccessfully);
         } while (!opponentPlacedSuccessfully);
 
-        System.out.println("Players Ships: ");
-        playersBoard.getShips().forEach(s -> {
-            System.out.println("\t" + s.getKind());
-        });
-
-        System.out.println("Enemy Ships: ");
-        opponentsBoard.getShips().forEach(s -> {
-            System.out.println("\t" + s.getKind());
-        });
-
-        System.out.println("Ending ship placements");
         return true;
     }
 
@@ -54,12 +35,8 @@ public class Game {
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
     public boolean attack(int x, char  y) {
-        System.out.println("");
-        System.out.println("");
-        System.out.println("Attack request came in!");
-        System.out.println("Player is attempting to attack...");
         Result playerAttack = opponentsBoard.attack(x, y);
-        if (playerAttack.getResult() == AtackStatus.INVALID) {
+        if (playerAttack.getResult() == INVALID) {
             return false;
         }
 
@@ -67,30 +44,22 @@ public class Game {
         do {
             // AI does random attacks, so it might attack the same spot twice
             // let it try until it gets it right
-            System.out.println("Enemy is attempting to attack...");
             opponentAttackResult = playersBoard.attack(randRow(), randCol());
-        } while(opponentAttackResult.getResult() == AtackStatus.INVALID);
+        } while(opponentAttackResult.getResult() == INVALID);
 
-
-        System.out.println("Ending attacks");
         return true;
     }
 
     private char randCol() {
-        Random rand = new Random();
-        char randchar = (char)(rand.nextInt(10) + 'A');
-        return randchar;
+        int random = new Random().nextInt(10);
+        return (char) ('A' + random);
     }
 
     private int randRow() {
-        Random rand = new Random();
-        int randnum = rand.nextInt(10)+1;
-        return randnum;
+        return  new Random().nextInt(10) + 1;
     }
 
     private boolean randVertical() {
-        Random rand = new Random();
-        boolean tf = rand.nextBoolean();
-        return tf;
+        return new Random().nextBoolean();
     }
 }
