4c6ae6c571216e8a0f262458e7ccbecc18122d0e
mitch <mitch.keys1@gmail.com>
2019-02-20 13:44:13 -0800

    its about damn time guys

--- src/main/java/cs361/battleships/models/Game.java
+++ src/main/java/cs361/battleships/models/Game.java
@@ -2,11 +2,9 @@ package cs361.battleships.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
-import static cs361.battleships.models.AtackStatus.*;
+import static cs361.battleships.models.AttackStatus.*;
 
 public class Game {
 
